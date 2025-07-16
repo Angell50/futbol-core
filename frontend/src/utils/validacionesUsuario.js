faf0806eadf144dca36327cd9ca60062fee941fb
Angell50 <angel.vela@udla.edu.ec>
2025-07-15 19:00:46 -0500

    Actualización: front

--- frontend/src/utils/validacionesUsuario.js
+++ frontend/src/utils/validacionesUsuario.js
@@ -8,9 +8,23 @@ const ValidacionesUsuario = (() => {
     return contraseña && contraseña.length >= 6;
   };
 
+  const validar = ({ correo, contraseña, rol }) => {
+    if (!esCorreoValido(correo)) {
+      return 'Correo inválido.';
+    }
+    if (!esContraseñaSegura(contraseña)) {
+      return 'La contraseña debe tener al menos 6 caracteres.';
+    }
+    if (!rol) {
+      return 'Debe seleccionar un rol.';
+    }
+    return null;
+  };
+
   return {
     esCorreoValido,
     esContraseñaSegura,
+    validar, // <- Aquí exportas la función
   };
 })();
 
